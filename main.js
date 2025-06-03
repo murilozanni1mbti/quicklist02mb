87bf251dd71171ffd0544f1fc5efd2ae29f03009
murilo zanni <murilozanni@gmail.com>
2025-06-03 19:01:31 +0000

    termino do trabalho

--- main.js
+++ main.js
@@ -1,85 +1,82 @@
-const item = []
-let items = []
+let items = []     
 
-function addItem(){
+function addItem() {
     const itemName = document.querySelector("#item").value
 
- if (itemName === "") {Add commentMore actions
+     if (itemName === "") {
         alert("Não é possível adicionar um item em branco!")
         return
     }
-
-
+    
     const item = {
         name: itemName,
         checked: false
     }
 
-    item.push(item)
+    items.push(item)
 
     document.querySelector("#item").value = ""
 
-showItemList
+    showItemsList()
 }
 
-function showItemList(){
+function showItemsList() {
     const sectionList = document.querySelector(".list")
     sectionList.textContent = ""
-  items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
-    showItemList.map((item, index)=> {
-     sectionList.innerHTML += `
-   <div class="item">
+     items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+        <div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}">
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked ? "checked" : ""}>
                     <div class="custom-checkbox" onClick="checkItem('${item.name}')">
-                      <img src="./assets/checked.svg" alt="checked">
+                        <img src="./assets/checked.svg" alt="checked">
                     </div>
-                    <label for="item-${index}"> onClick="checkItem('${item.name}')"${item.name}</label>
+                    <label for="item-${index}" onClick="checkItem('${item.name}')">${item.name}</label>
                 </div>
-                    
-                <button>
-                    <img src="./assets/trash-icon.svg" alt="trash-icon">
+
+                <button onClick="removeItem('${item.name}')">
+                    <img src="./assets/trash-icon.svg" alt="trash icon">
                 </button>
             </div>
-            `
+        `
     })
+
+     localStorage.setItem("items", JSON.stringify(items))
 }
 
-
 function removeItem(itemName) {
- const divWarning = document.querySelector(".warning")
     const itemIndex = items.findIndex((item) => item.name === itemName)
- divWarning.classList.remove("hide-warning")
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
+
     if (itemIndex !== -1) {
-         setTimeout(() => { 
-      divWarning.classList.add("hide-warning")}
- 4000) }
-   
+        items.splice(itemIndex, 1)
+    }
 
+    showItemsList()
+}
 
-  showItemsList()
-  }
-
-
-  function checkItem(itemName) {
+function checkItem(itemName) {
     const itemIndex = items.findIndex((item) => item.name === itemName)
-    items.checked = !item.checked
-  }
-      items[itemIndex].checked = !items[itemIndex].checked
-
-
-
-      Add commentMore actions
+    items[itemIndex].checked = !items[itemIndex].checked
+    showItemsList()
+}
 
 function addHideWarningClass() {
     document.querySelector(".warning").classList.add("hide-warning")
 }
 
-  localStorage.setItem("items", JSON.stringify(items))
 function verifyLocalStorageItems() {
     const localStorageItems = localStorage.getItem("items")
 
-    if (localStorage) {
+    if (localStorageItems) {
         items = JSON.parse(localStorage.items)
         showItemsList()
     }
